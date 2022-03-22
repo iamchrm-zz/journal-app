ed2ba0c52ae46485dd7999d92f4c4f45800bcf18
Christian Messina <chrmesina@icloud.com>
2022-03-22 19:23:12 -0300

    prod v.0.1

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -9,12 +9,7 @@ export const RegisterScreen = () => {
   const dispatch = useDispatch();
   const { msgError } = useSelector((state) => state.ui);
 
-  const [formValues, handleInputChange] = useForm({
-    name: "Hernando",
-    email: "nando@gmail.com",
-    password: "123456",
-    password2: "123456",
-  });
+  const [formValues, handleInputChange] = useForm();
 
   const { name, email, password, passwordConfirm } = formValues;
 
